2ba22def3ca43523b31e9ddcc35b84f6cc8baef9
dieryVale <dieryvale.01@gmail.com>
2024-11-22 03:28:31 -0500

    diseño

--- networkly/networkly_app/views.py
+++ networkly/networkly_app/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate, login
-
+from .forms import ProfileForm ##
+from .models import Profile##
 
 # Create your views here.
 
@@ -38,5 +39,28 @@ def register(request):
             return redirect('home')
 
     return render(request, 'registration/register.html', data)
+
+##
+def profile_view(request):
+    try:
+        # Obtener el perfil asociado al usuario
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = None
+
+    if request.method == 'POST':
+        # el formulario es POST, actualizamos el perfil
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')  # Redirige a la página del perfil después de guardar
+    else:
+        #un GET, mostramos el perfil y el formulario de edición
+        form = ProfileForm(instance=profile)
+
+    return render(request, 'networkly_app/perfil.html', {'profile': profile, 'form': form})
+##
+
+
 
 
